2bd92ad41c874a364cfcaa376f450fa323988ce7
Avery Bush <abush@uoregon.edu>
2020-04-24 10:04:54 -0700

    finished functions.js

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,15 +1,90 @@
 // jshint esversion: 6
 
+//3a
+function reverseWord(w) {
+  //return word but reversed
+  let splitString = w.split("");
+  let reversedArray = splitString.reverse();
+  let joinArray = reversedArray.join("");
+  for (let j = 0; j < w.length; j++) {
+  }
+  return joinArray;
+}
+
+let revWords1 = (s) => {
+  let wordArray = s.split(" "),
+    result ="";
+
+  for (let i = 0; i < wordArray.length; i++){
+    let currentWord = wordArray[i];
+    let reversedWord = reverseWord(currentWord);
+    //change result
+      result = result + reversedWord + " ";
+  }
+  return result;
+};
+
+//3b
+function reverseWord(w) {
+  //return word but reversed
+  let splitString = w.split("");
+  let reversedArray = splitString.reverse();
+  let joinArray = reversedArray.join("");
+  for (let j = 0; j < w.length; j++) {
+  }
+  return joinArray;
+}
+
+let revWords2 = (s) => {
+  let wordArray = s.split(" "),
+    result ="";
+
+  wordArray.forEach(function(item) {
+    let currentWord = item;
+    let reversedWord = reverseWord(currentWord);
+    //change result
+      result = result + reversedWord + " ";
+  });
+  return result;
+};
+
+//3c
+function reverseWord(w) {
+  //return word but reversed
+  let splitString = w.split("");
+  let reversedArray = splitString.reverse();
+  let joinArray = reversedArray.join("");
+  for (let j = 0; j < w.length; j++) {
+  }
+  return joinArray;
+}
+
+let revWords3 = (s) => {
+  let wordArray = s.split(" "),
+    result ="";
+
+  for (item of wordArray) {
+    let currentWord = item;
+    let reversedWord = reverseWord(currentWord);
+    //change result
+      result = result + reversedWord + " ";
+  }
+  return result;
+};
+
+//5 xc
 let containsDuplicates = s => {
-  let cArr = [0, 0, 0, 0],
+  let word = s.split("");
+  //let cArr = [0, 0, 0, 0],
+  let cArr = new Array(26).fill(0),
     code;
-  for (let i in cArr) {
-    code = s[i].toUpperCase().charCodeAt(0) - 65;
+  for (var i = 0; i < s.length; i++) {
+    code = word[i].toUpperCase().charCodeAt(0) - 65;
     //console.log(code);
     if (cArr[code] > 0) {
       return true;
     } else {
-      cArr[i]++;
+      cArr[code]++;
     }
   }
   return false;
