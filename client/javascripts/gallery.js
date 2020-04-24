9beb9bfb1aa8190ab6f46dc6c9eb3f0331012b62
Avery Bush <abush@uoregon.edu>
2020-04-24 10:03:04 -0700

    gallery.html, gallery.js, and gallery.css

--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,15 +1,22 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function(){
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+    let prefixURL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
     let suffixURL = "&format=json&jsoncallback=?";
     //get value entered by user from textbox
-    let flickrTag = $("input").???();
+    //let flickerTag = document.querySelector('input[type=text]').value;
+    //WHAT 3 LETTERS GO IN THE ???
+  //  let flickrTag = document.querySelector("input[type=text]").value();
+  let flickrTag = $("input").val();
+    console.log(flickrTag);
+    //  let flickrTag = $("input").v??();
     let requestURL = prefixURL + flickrTag + suffixURL;
+    console.log(requestURL);
 
     //clear old photos
-    $(".photos").???("");
+    //document.querySelector(".photos").innerHTML ="";
+    $(".photos").html("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
@@ -26,7 +33,7 @@ let main = function(){
         $img.attr("src", item.media.m);
         $img.attr("width", "100");
 
-        // attach the img tag to the main
+        // attach the img tag to the controller
         // photos element and then fade it in
         $(".photos").append($img);
         $img.fadeIn();
@@ -37,4 +44,13 @@ let main = function(){
 };
 
 
-$(document).ready(main);
+
+//$(document).ready(controller);
+
+                                                                                                                                                                                                                                                                                                                      // NOTE:    // NOTE:
+//without using jQuery
+window.addEventListener("load", () => {
+ //select the button and register the handler
+ let button = document.querySelector("button");
+ button.addEventListener("click", controller);
+});
